088c36a629304f44722e18995f07b4fcb571aa38
clauhp <claudia8838@homail.es>
2019-04-25 00:58:18 +0200

    Still trying

--- server.py
+++ server.py
@@ -2,14 +2,27 @@ import http.server
 import http.client
 import socketserver
 import termcolor
-import requests
+from ensembl_info import speciesnames, get_karyotype, get_chlength
 
 
-PORT = 8080
+PORT = 8088                     #CHANGE PROT CHANGE PORT TO 8000
 
 HOSTNAME = 'rest.ensembl.org'
 ENDPOINTS = ['/info/species', '/info/assembly']
 header = {"Content-Type": "application/json"}
+
+htmlfile = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Response</title>
+</head>
+<body style="margin-left:50px">
+  <h1>{}</h1>
+  <p>{}</p>
+  <a href="/">Main page</a>
+</body>
+</html>"""
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -21,48 +34,60 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         path = self.path
         contents = ""
+        out = ""
+        pathlist = path.split('=')
 
         if path == "/":
             f = open("mainpage.html", 'r')
             contents = f.read()
 
-        elif path == "/listSpecies":
-            r = requests.get(HOSTNAME + ENDPOINTS[0], headers=header)
-            decoded = r.json()
+        elif path.startswith("/listSpecies"):
+            f = open('output.html', 'w')
+            limit = int(pathlist[1])
+            species = speciesnames()
+            h = "List of Species"
 
-            limit = path.split('=')[1]
+            if len(pathlist) == 2:                         # AQUI HAY ALGO QUE NO CHUTA
+                species = species[:limit]
+            elif len(pathlist) == 1:
+                species = species
 
-            number_species = decoded['species']
-            species = []
+            for i in range(len(species)):
+                out += species[i] + "<br>"
 
-            for number in number_species:
-                name = number['common_name']
-                species.append(name)
+            output = htmlfile.format(h, out)
+
+            f.write(output)
+            f.close()
+            d = open('output.html', 'r')
+            contents = d.read()
+
+        elif path.startswith("/karyotype"):
+            f = open('output.html', 'w')
+
+            msg = pathlist[1]
+            karyotype = get_karyotype(msg)
+            h = "Karyotype"
+
+            for i in range(len(karyotype)):
+                out += karyotype[i] + ', '              # THERE IS A FUCKING COMA I CANNOT GET RID OF
+
+            output = htmlfile.format(h, out)
+
+            f.write(output)
+            f.close()
+            d = open('output.html', 'r')
+            contents = d.read()
 
 
-            content = """<!DOCTYPE html>
-            <html lang="en">
-            <head>
-              <meta charset="utf-8">
-              <title>listSpecies</title>
-            </head>
-            <body>
-              <h1>List of all species</h1>
-              <p>Sequence: {}</p>
-              <p></p>
-              <p>{}</p>
-              <p></p>
-              <a href="/">Main page</a>
-            </body>
-            </html>"""
 
-
-
-        elif path == "/karyotype":
+        elif path.startswith("/chromosomeLength"):
             pass
 
-        elif path == "/chromosomeLength":
-            pass
+        else:
+            d = open("error.html", 'r')
+            contents = d.read()
+
 
         self.send_response(200)
 
@@ -72,7 +97,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
         # --Sending the body of the response message
         self.wfile.write(str.encode(contents))
-
 
 
 with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
